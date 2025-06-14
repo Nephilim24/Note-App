1bdc43f4223ae7d4660c0b620656af40aef045e6
student <student@intranet.proweb.uz>
2025-06-14 20:53:22 +0500

    NavBar not full logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Main } from "./widgets/Main/Main.jsx";
+import { useState } from "react";
 import { NavBar } from "./widgets/NavBar/NavBar.jsx";
 
 const App = () => {
+
+  const [searchMode, setSearchMode] = useState(false);
+
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar searchMode={searchMode} setSearchMode={setSearchMode} />
     </>
-  )
+  );
 }
 
 export default App;
